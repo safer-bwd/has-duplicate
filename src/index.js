da0535a2d85e2b91aaa82b03a5aafca01cf99ce9
safer <asafron2@ford.com>
2019-09-05 16:13:57 +0300

    feature: implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import isEqual from 'lodash.isequal';
+import findIndexesOfDuplicates from 'find-indices-of-duplicates';
 
 export default (array, comparator = isEqual) => {
   if (array.length === 0) {
     return false;
   }
 
-  return false;
+  const duplicates = findIndexesOfDuplicates(array, comparator, {
+    onlyFirst: true
+  });
+
+  return duplicates.length > 0;
 };
