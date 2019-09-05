e996a425622a77e54488dcdf9e79dbc49d1afae5
safer <asafron2@ford.com>
2019-09-05 16:27:11 +0300

    docs: add description and api docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import isEqual from 'lodash.isequal';
 import findIndexesOfDuplicates from 'find-indices-of-duplicates';
 
+/**
+ * @name has-duplicate
+ * @param {Array} array The array to check
+ * @param {Function} [comparator=lodash.isequal] The compare function
+ * @return {boolean} True if has duplicates and false otherwise
+ * @example
+ * import hasDuplicates from 'has-duplicate';
+ * */
 export default (array, comparator = isEqual) => {
   if (array.length === 0) {
     return false;
