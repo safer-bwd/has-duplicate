0594f19233060afd7e86d19992920f8cd3c04206
safer <asafron2@ford.com>
2019-09-05 16:04:03 +0300

    test: add tests

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,4 +1,16 @@
 import hasDuplicate from '../src';
 
-it('test', async () => {
+it('should work with a default `comparator`', async () => {
+  expect(hasDuplicate([])).toBeFalsy();
+  expect(hasDuplicate([1, 2, 3])).toBeFalsy();
+  expect(hasDuplicate([1, 2, 3, 1])).toBeTruthy();
+  expect(hasDuplicate([{ v: 1 }, 2, 3, { v: 1 }])).toBeTruthy();
+});
+
+it('should work with a custom `comparator`', async () => {
+  expect(hasDuplicate([], () => true)).toBeFalsy();
+  const comparator = (a, b) => a === b;
+  expect(hasDuplicate([1, 2, 3], comparator)).toBeFalsy();
+  expect(hasDuplicate([1, 2, 3, 1], comparator)).toBeTruthy();
+  expect(hasDuplicate([{ v: 1 }, 2, 3, { v: 1 }], comparator)).toBeFalsy();
 });
